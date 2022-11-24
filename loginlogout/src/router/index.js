018daed1ccbd45186b705835cc90e6e672ed20af
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2022-11-24 17:10:10 -0300

    tela de login

---
+++ loginlogout/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+import Login from '../views/LoginLogout.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ loginlogout/src/views/LoginLogout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="primeira">
+      <form class="form">
+        <h1>Cadastrar</h1>
+
+        <label>
+          <span>Seu e-mail:</span>
+          <input type="email" name="email" />
+        </label>
+
+        <label>
+          <span>Senha:</span>
+          <input type="password" name="password" />
+        </label>
+
+        <button class="submit" type="submit">Cadastrar</button>
+      </form>
+    </div>
+
+    <div class="segunda">
+      <form class="form">
+        <h1>Logar:</h1>
+
+        <label>
+          <span>Seu e-mail:</span>
+          <input type="email" name="email" />
+        </label>
+
+        <label>
+          <span>Senha:</span>
+          <input type="password" name="password" />
+        </label>
+
+        <button class="submit" type="submit">Cadastrar</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
